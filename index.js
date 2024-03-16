f427067517ad80274704725ef35e2452a7e3905c
GGTuran <tayyabturan67@gmail.com>
2024-03-16 15:22:49 +0600

    started loading all data and single data separetly

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const phones = require("./phones.json");
 const app = express();
 const port = 4000;
 
@@ -6,9 +7,19 @@ app.get('/', (req,res) =>{
     res.send("Hello from the better side of the world");
 })
 
+app.get("/phones",(req,res) =>{
+    res.send(phones);
+})
+
+app.get("/phones/:id", (req,res) =>{
+    const id = parseInt(req.params.id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone);
+} )
+
 
 app.get("/data", (req, res) =>{
-    res.send("need to assign some data asap");
+    res.send("need to assign some data asap i learn how to load");
 })
 
 app.listen(port, () =>{
