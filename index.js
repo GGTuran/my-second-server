824fc5f0a9becb1e126a00bbe81fabe6cff605fe
GGTuran <tayyabturan67@gmail.com>
2024-03-16 15:08:58 +0600

    exploring express js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req,res) =>{
+    res.send("Hello from the better side of the world");
+})
+
+
+app.get("/data", (req, res) =>{
+    res.send("need to assign some data");
+})
+
+app.listen(port, () =>{
+    console.log(`Serving from port: ${port}`)
+} )
