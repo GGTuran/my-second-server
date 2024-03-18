c29180aa3dfc78798e08157a7fe54c6942d1e7a3
GGTuran <tayyabturan67@gmail.com>
2024-03-18 19:02:20 +0600

    card adding

--- Components/Card.jsx
+++ Components/Card.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>
-            
-        </div>
+        <label className="swap swap-flip text-9xl">
+  
+        {/* this hidden checkbox controls the state */}
+        <input type="checkbox" />
+        
+        <div className="swap-on">😈</div>
+        <div className="swap-off">😇</div>
+      </label>
     );
 };
 
