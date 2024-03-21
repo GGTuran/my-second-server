b7c7553b832eaa71e3f1d43361c11f7abc5d38b5
GGTuran <tayyabturan67@gmail.com>
2024-03-21 19:14:54 +0600

    indicator

---
+++ Components/Indicator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Indicator = () => {
+    return (
+        <div className="indicator">
+  <span className="indicator-item indicator-center indicator-middle badge badge-secondary">Uploading Image...</span> 
+  <img alt="Tailwind CSS examples" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" />
+</div>
+    );
+};
+
+export default Indicator;
