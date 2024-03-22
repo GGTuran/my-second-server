1316a42f6ad2198783aacca97337bbd67f8ba88b
GGTuran <tayyabturan67@gmail.com>
2024-03-22 19:27:28 +0600

    swap

--- Components/Chats.jsx
+++ Components/Chats.jsx
@@ -19,6 +19,14 @@ const Chats = () => {
   </ul>
 </div>
 
+<label className="swap swap-flip text-9xl">
+  
+  {/* this hidden checkbox controls the state */}
+  <input type="checkbox" />
+  
+  <div className="swap-on">😈</div>
+  <div className="swap-off">😇</div>
+</label>
 
 
 
